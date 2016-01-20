69f1c08aa1454760abe26687cfa7b777b4ed6845
joshR2 <josh2cool365@aol.com>
2016-01-20 08:02:11 -0700

    First C++ Project Working with console.

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,27 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: jrob2049
+ */
+
+/**
+ *
+ */
+
+#include <iostream>	//Using input and output streams.
+using namespace std; //Access to the Standard library.
+int main()
+{
+	cout << "This is how you type to the console. in C++" << endl;
+	//This would be like System.out.println("sadjkgewagl");
+	int temp;
+
+	cin >> temp;
+
+	cout <<temp;
+
+	cin >> 1
+
+	return 0;
+}
